4c7c7b072390e3c450f00f1e3721a06388fc9b5f
yjpurple <yjpurple@gmail.com>
2024-03-29 17:08:21 +0900

    test3333

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,10 +4,11 @@ import { Link } from 'react-router-dom';
 function Header(props) {
 	return (
 		<>
-			<h1>REACT-APP</h1>
+			<h1>TEST3</h1>
 			<ul>
 				<Link to="/profile/1"><li>1번 Profile</li></Link>
 				<Link to="/product/1"><li>product 1번 상품</li></Link>
+				<Link to="/accordion/1"><li>Accordion 1번 상품</li></Link>
 				<Link to="/sign/1"><li>로그인 하기</li></Link>
 			</ul>
 		</>
