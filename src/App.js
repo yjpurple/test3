1f6a539c34a8e8b9f8da9621050be027a175d241
yjpurple <yjpurple@gmail.com>
2024-03-29 17:04:16 +0900

    test333

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import Main from './pages/Main';
+import Accordions from './pages/Accordion';
 import NotFound from './pages/NotFound';
 import Product from './pages/Product';
 
@@ -9,12 +10,13 @@ import './App.scss';
 
 const App = () => {
 	return (
-		<div className='App'>
+		<div className='container'>
 		<BrowserRouter>
 			<Header />
 			<Routes>
 				<Route path="/" element={<Main />}></Route>
 				<Route path="/product/*" element={<Product />}></Route>
+				<Route path="/accordion/*" element={<Accordions />}></Route>
 				{/* 상단에 위치하는 라우트들의 규칙을 모두 확인, 일치하는 라우트가 없는경우 처리 */}
 				<Route path="*" element={<NotFound />}></Route>
 			</Routes>
