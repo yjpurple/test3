f5025d0e0df485af55c9f542db8529050e2646cb
yjpurple <yjpurple@gmail.com>
2024-03-29 17:11:15 +0900

    test5555

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ const App = () => {
 				<Route path="/" element={<Main />}></Route>
 				<Route path="/product/*" element={<Product />}></Route>
 				<Route path="/accordion/*" element={<Accordions />}></Route>
-				{/* 에 위치하는 라우트들의 규칙을 모두 확인, 일치하는 라우트가 없는경우 처리 */}
+				{/* 상단에 위치하는 라우트들의 규칙을 모두 확인, 일치하는 라우트가 없는경우 처리 */}
 				<Route path="*" element={<NotFound />}></Route>
 			</Routes>
 		</BrowserRouter>
